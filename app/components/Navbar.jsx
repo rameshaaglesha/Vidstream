8b6d4ebc67b4f66c2941e5d6a50c6e146360c4fe
Ramesh Kumar <51584053+rameshaaglesha@users.noreply.github.com>
2025-09-10 12:22:41 +0530

    commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+        <header className='flex justify-center py-8'>
+            <h1 className='font-extrabold text-transparent text-2xl sm:text-3xl md:text-6xl bg-clip-text bg-gradient-to-r from-red-600 to-blue-900'>VideoStream</h1>
+        </header>
+    </>
+  )
+}
